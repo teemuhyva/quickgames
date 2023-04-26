1a7562bc6e43806641b2f31d7e859397f05b034c
Teemu Hyvärinen <g9621@student.jamk.fi>
2023-04-26 07:52:19 +0300

    updated file path

--- Quicktournament/App.tsx
+++ Quicktournament/App.tsx
@@ -2,7 +2,7 @@
 import { NavigationContainer } from '@react-navigation/native';
 import type { ReactNode } from 'react';
 import React from 'react';
-import BottomTabs from './src/navigator/TabNavigator';
+import BottomTabs from './src/Navigator/TabNavigator';
 //import { RealmContext } from './src/models/RealmConfig';
 import RealmContext from '../Quicktournament/src/models/RealmConfig'
 
